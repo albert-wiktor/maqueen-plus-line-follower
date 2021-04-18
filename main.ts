4948d15b52a22596101facc35f3ee069204e591b
Albert & Wiktor <79414658+albert-wiktor@users.noreply.github.com>
2021-04-18 17:08:33 +0200

    parking sensor + automatic emergency braking system

--- main.ts
+++ main.ts
@@ -1,7 +1,22 @@
+let distance = 0
 DFRobotMaqueenPluss.I2CInit()
 DFRobotMaqueenPluss.PID(PID.OFF)
 basic.forever(function () {
-    if (DFRobotMaqueenPluss.readPatrol(Patrol.L3) == 1) {
+    distance = DFRobotMaqueenPluss.ultraSonic(PIN.P1, PIN.P2)
+    if (distance == 0) {
+        distance = 1000
+    } else if (distance < 20) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    } else if (distance < 100) {
+        basic.pause(distance * 8)
+        music.playTone(262, music.beat(BeatFraction.Sixteenth))
+    }
+})
+basic.forever(function () {
+    if (distance < 20) {
+        DFRobotMaqueenPluss.mototStop(Motors.ALL)
+        basic.pause(250)
+    } else if (DFRobotMaqueenPluss.readPatrol(Patrol.L3) == 1) {
         DFRobotMaqueenPluss.mototRun(Motors.M1, Dir.CCW, 40)
         DFRobotMaqueenPluss.mototRun(Motors.M2, Dir.CW, 40)
         basic.pause(100)
